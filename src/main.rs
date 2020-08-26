226ac1dbc0be6290d0bc960f559e934de27c9cd5
Jae Anne Bach Hardie <jhardie@findmypast.com>
2020-08-27 00:37:49 +0100

    It reads a file now, that's progress.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() -> io::Result<()> {
+    let f = File::open("Cargo.toml")?;
+    let reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }
