331685590dee22c8a959e55f6eed7d18c010c2eb
Jae Anne Bach Hardie <jhardie@findmypast.com>
2020-08-30 21:31:59 +0100

    Now creates a SQLite database

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use std::fs::File;
+use rusqlite::Connection;
+use std::fs;
 use std::io;
-use std::io::prelude::*;
-use std::io::BufReader;
 
-fn main() -> io::Result<()> {
-    let f = File::open("Cargo.toml")?;
-    let reader = BufReader::new(f);
+fn create_database() -> rusqlite::Result<Connection> {
+    let ribbit_path = "./.ribbit";
+    match fs::metadata(&ribbit_path) {
+        Ok(_) => (),
+        Err(error) => match error.kind() {
+            io::ErrorKind::AlreadyExists => fs::create_dir(&ribbit_path).unwrap(),
+            other_error => panic!(other_error),
+        },
+    };
+    let db = Connection::open("./.ribbit/repo.db")?;
+    db.execute(
+        "create table if not exists changes (
+            id text primary key,
+            date_created integer not null,
+            revision_id text not null,
+            file_path text not null
+        )",
+        rusqlite::NO_PARAMS,
+    )?;
+    return Ok(db);
+}
 
-    for line in reader.lines() {
-        println!("{}", line?);
-    }
+fn main() -> rusqlite::Result<()> {
+    let _db = create_database()?;
 
     Ok(())
 }
